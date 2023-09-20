a58873d32e62d38f3d79bca0ab2c6a4570cb6263
Pratham Doshi <37569372+Prathamdoshi@users.noreply.github.com>
2023-09-20 14:52:37 -0700

    Project Complete

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import requests
+from datetime import date
+import  datetime as dt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# ------------------------------ PROFILE ------------------------------ #
+
+GENDER = "male"
+WEIGHT = 86.1
+HEIGHT = 180.34
+AGE = 28
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+headers = {
+    "x-app-id" : "462425a2",
+    "x-app-key" : "5372c30359c4eb09d6b0d5ff55cd1c58"
+}
+
+workout_data = {
+ "query":"",
+ "gender": GENDER,
+ "weight_kg": WEIGHT,
+ "height_cm": HEIGHT,
+ "age": AGE
+}
+
+workout_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+g_sheet_endpoint = "https://api.sheety.co/7841abbd283b105c897f80243e2cbdf2/myWorkouts/workouts"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# g_reponse = requests.post(url=g_sheet_endpoint,json=g_data)
+
+user_response = input("What workout did you do today? ")
+
+workout_data["query"] =  user_response
+
+workout_response = requests.post(url=workout_endpoint,json=workout_data,headers=headers)
+
+excercises = workout_response.json()['exercises']
+
+for workout in excercises:
+
+    g_data = {
+        "workout": {
+            "date": str(date.today()),
+            "time": str(dt.datetime.now()),
+            "exercise": workout['user_input'],
+            "duration": workout["duration_min"],
+            "calories": workout["nf_calories"]
+        }
+    }
+
+    g_reponse = requests.post(url=g_sheet_endpoint,json=g_data)
+
